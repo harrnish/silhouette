413eefeeb03a7d4881a5565ff46adfe29c316785
Harnish <harrnish@gmail.com>
2024-10-08 19:26:30 -0400

    Index - Grid redefined

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,18 +1,44 @@
 import React from "react";
 import "./Index.css";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 const Index = () => {
   const items = Array.from({ length: 80 }, (_, i) => `Item ${i + 1}`);
+  const heights = [80, 90, 100, 110, 120, 130];
+  const colors = [
+    "#eeeeee",
+    "#000000",
+    "#999999",
+    "#cdcdcd",
+    "#292929",
+    "#3e3e3e",
+  ];
+
+  const getRandomHeight = () => {
+    return heights[Math.floor(Math.random() * heights.length)];
+  };
+
+  const getRandomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
 
   return (
-    <div className="index">
-      <div className="grid-container">
-        <div className="grid-item logo"></div>
-        {items.map((item, index) => (
-          <div key={index} className="grid-item"></div>
-        ))}
+    <ReactLenis root>
+      <div className="index">
+        <div className="grid-container">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="grid-item"
+              style={{
+                height: `${getRandomHeight()}px`,
+                backgroundColor: getRandomColor(),
+              }}
+            ></div>
+          ))}
+        </div>
       </div>
-    </div>
+    </ReactLenis>
   );
 };
 
